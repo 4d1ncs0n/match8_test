d93e59b29cc94939e3707676c9953c72ef7a274f
Danny Velasquez <adincson@gmail.com>
2021-12-01 01:46:43 -0500

    feat: application working

---
+++ app.py
@@ -0,0 +1,54 @@
+import requests
+
+
+def get_data(source:str) -> list:
+    rawdata = requests.get(source).json()
+    data = rawdata['values']
+    return data
+
+def find_target(target:int, point:int, data:list) -> object:
+    player = data[point]
+    
+    if int(player['h_in']) == target:
+        return ' '.join([player['first_name'], player['last_name']]), point
+    
+    if point == len(data)-1:
+        return False, False
+    
+    point +=1
+    
+    return find_target(target, point, data)
+
+def app(data:list, aim:int) -> None:
+    i = 0
+    final = len(data)-1
+    check = list()
+    while i <= final:
+        da = data[i]
+        target = aim-int(da['h_in'])
+        couple, point = find_target(target, point=i, data=data)
+        check.append(bool(couple))
+
+        if point is False:
+            i += 1
+        else:
+            
+            player = ' '.join([da['first_name'], da['last_name']])
+            if i == point:
+                i = point + 1
+            else:
+                print('\t'.join([player, couple]))
+                i = point
+    else:
+        if any(check) is False:
+            print('No matches found')
+
+
+if __name__ == '__main__':
+
+    source_data = 'https://mach-eight.uc.r.appspot.com/'
+    data = get_data(source_data)
+
+    aim = input('app ')
+    app(data, int(aim))
+
